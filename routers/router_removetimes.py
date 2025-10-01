a50d7d4c5585ea4b806840aec4d25b814d49e84f
洪瑞聪 <hongruicong@hongruicongs-MacBook-Air.local>
2025-10-02 00:28:26 +0800

    完成: 获取消课时get请求

--- routers/router_removetimes.py
+++ routers/router_removetimes.py
@@ -6,6 +6,7 @@ from handle_db.handle_course import get_by_id
 from handle_db.handle_removetimes import get
 from models.removetimes import RemoveTimes
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from public.const import weekday_dic
 
 
 collection = db['removetimes']
@@ -19,9 +20,9 @@ async def get_all():
         course = get_by_id(one.course_id)[0]
         records.append({
             'content': course.content,
-            'weekday': course.weekday,
-            'timerange': f'{course.course_time.start_time}-{course.course_time.end_time}',
+            'time': f'{weekday_dic[course.weekday]}{course.course_time.start_time}-{course.course_time.end_time}',
             'students': " ".join([s.en_name for s in course.students]),
             'course_left': course.course_left,
             'create_time': one.create_time,
         })
+    return records
